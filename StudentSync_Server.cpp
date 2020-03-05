0d67eb2a03d865bf358ee9212c26fd1aca3621c1
Kennedy J.J. Maturure <kenjjmat@outlook.com>
2020-03-05 11:28:51 +0200

    Update header

--- StudentSync_Server.cpp
+++ StudentSync_Server.cpp
@@ -1,9 +1,17 @@
 /*
-** SCS2206 Groupwork 2020
-** StudentSync Server
+** StudentSync LAN Synchronization Tool: Server
+**
+** This code may not be copied, modified or distributed without the
+** express written permission of the author(s). Any violation shall
+** be prosecuted to the maximum extent possible under law.
+**
+*************************************************************************
+** Project Details:
+**
+** National University of Science and Technology
+** SCS2206 Computing in Society Group Project
 **
 ** Group members: Alec T. Musasa, Kennedy J.J. Maturure
-**
 **
 */
 
